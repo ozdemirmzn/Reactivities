dc380921823b035d11129e5970db56b2fd0009cd
Ramazan Özdemir <ozdemirmzn@gmail.com>
2021-06-10 13:19:27 -0500

    layout of client app completed

---
+++ client-app/src/app/layout/App.tsx
@@ -0,0 +1,30 @@
+import React, { Fragment, useEffect, useState } from 'react';
+import axios from 'axios';
+import { Container, Header, List } from 'semantic-ui-react';
+import { Activity } from '../models/activity';
+import Navbar from './Navbar';
+import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
+
+
+function App() {
+
+  const [activities, setActivities] = useState<Activity[]>([]);
+
+  useEffect(() => {
+    axios.get<Activity[]>('http://localhost:5000/api/activities').then(response => {
+      
+      setActivities(response.data);
+    })
+  }, [])
+
+  return (
+    <>
+      <Navbar />
+      <Container style={{marginTop:'7em'}}>
+        <ActivityDashboard activities={activities}/>
+      </Container>
+    </>
+  );
+}
+
+export default App;
